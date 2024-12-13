59566981e96a6b9557620416368bd57746a8cff4
abraekev <arnout.braekevelt@gmail.com>
2024-12-13 23:27:52 +0100

    setting up clean css

--- frontend/front-office/src/app/App.tsx
+++ frontend/front-office/src/app/App.tsx
@@ -1,8 +1,7 @@
-import "./App.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { FC } from "react";
-import { First } from "./pages/first/first";
+import { First } from "../pages/first/first";
 
 // move this to a config
 const router = createBrowserRouter([
